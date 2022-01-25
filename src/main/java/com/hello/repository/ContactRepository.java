0283bc81c2ffe88b4595416cfdc87e6aff12727b
velovan <velovan@users.noreply.github.com>
2022-01-25 12:10:54 +0200

    Update ContactRepository.java

--- src/main/java/com/hello/repository/ContactRepository.java
+++ src/main/java/com/hello/repository/ContactRepository.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import com.hello.entity.Contact;
  
-public interface ContactRepository {
+public interface ContactRepository{
 
     List<Contact> getFilteredContacts(String regex, boolean forward, long lastId, int limit);
+ 
 }
