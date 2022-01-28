c14d29caa943e30149682b27eded2fea5d0bfd59
velovan <velovan@users.noreply.github.com>
2022-01-28 12:52:42 +0200

    Update HelloBackEndApplication.java

--- src/main/java/com/hello/HelloBackEndApplication.java
+++ src/main/java/com/hello/HelloBackEndApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloBackEndApplication {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(HelloBackEndApplication.class, args);
     }
+    
 }
